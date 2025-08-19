8ae75b4e009733507f96f63235ede768ffc545d4
aakashmohole <aakashmohole@gmail.com>
2025-08-19 23:39:35 +0530

    chat oprations

--- main-backend-service/chat_config/models.py
+++ main-backend-service/chat_config/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, default=uuid.uuid4)
+    user_id = models.IntegerField()  
+    dataset = models.URLField(max_length=5000)
+    name_generated = models.BooleanField(default=False)  # ✅ Changed to Boolean
+    created_At = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
